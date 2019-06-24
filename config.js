3644b819d79db6be6cebb8337908ce184ecad0c1
girst <girst@users.noreply.github.com>
2019-06-24 16:06:59 +0200

    remove boot.jsm

--- config.js
+++ config.js
@@ -9,5 +9,10 @@ try {
     "ALLOW_LEGACY_EXTENSIONS": true,
   };
 
-  Cu.import('chrome://legacy/content/boot.jsm'); // engage the bootstrap loader
-} catch(ex) {}
+  // engage the bootstrap loader:
+  const {AddonManager} = Cu.import('resource://gre/modules/AddonManager.jsm');
+  const {BootstrapLoader} = Cu.import('chrome://legacy/content/BootstrapLoader.jsm');
+  AddonManager.addExternalExtensionLoader(BootstrapLoader);
+} catch(ex) {
+  Components.utils.reportError(ex.message);
+}
--- legacy/boot.jsm
+++
@@ -1,7 +0,0 @@
-let EXPORTED_SYMBOLS = [];
-
-const {AddonManager} = ChromeUtils.import('resource://gre/modules/AddonManager.jsm');
-if (AddonManager.addExternalExtensionLoader) {
-  const {BootstrapLoader} = ChromeUtils.import('chrome://legacy/content/BootstrapLoader.jsm');
-  AddonManager.addExternalExtensionLoader(BootstrapLoader);
-}
