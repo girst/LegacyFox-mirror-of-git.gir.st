e0222c6ba41459b5638ecf880fdc88d70128f6f9
girst <girst@users.noreply.github.com>
2019-06-24 16:50:00 +0200

    remove superfluous comments

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // keep this comment
 try {
-  // monkey-patching to disable signing and force-enable legacy extensions:
   let Xdb = Cu.import('resource://gre/modules/addons/XPIDatabase.jsm', {});
   Xdb.XPIDatabase['SIGNED_TYPES'].clear();
   Xdb.AddonSettings = {
@@ -9,7 +8,6 @@ try {
     "ALLOW_LEGACY_EXTENSIONS": true,
   };
 
-  // engage the bootstrap loader:
   const {AddonManager} = Cu.import('resource://gre/modules/AddonManager.jsm');
   const {BootstrapLoader} = Cu.import('chrome://legacy/content/BootstrapLoader.jsm');
   AddonManager.addExternalExtensionLoader(BootstrapLoader);
