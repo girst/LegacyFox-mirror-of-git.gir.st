aa3370144d450f4671919eaed205215f27482b7f
girst <girst@users.noreply.github.com>
2019-07-09 21:03:40 +0200

    fix chrome.manifest loading for mozilla68
    the chrome.manifest inside the install directory is not loaded any
    longer by default; see bugzil.la/1543761

--- config.js
+++ config.js
@@ -8,6 +8,12 @@ try {
     "ALLOW_LEGACY_EXTENSIONS": true,
   };
 
+  let manifest = Cc['@mozilla.org/file/directory_service;1']
+    .getService(Ci.nsIProperties).get('GreD', Ci.nsIFile);
+  manifest.append('chrome.manifest');
+  Components.manager.QueryInterface(Ci.nsIComponentRegistrar)
+    .autoRegister(manifest);
+
   const {AddonManager} = Cu.import('resource://gre/modules/AddonManager.jsm');
   const {BootstrapLoader} = Cu.import('chrome://legacy/content/BootstrapLoader.jsm');
   AddonManager.addExternalExtensionLoader(BootstrapLoader);
