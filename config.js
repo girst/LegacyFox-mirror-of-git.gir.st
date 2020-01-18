54655696e65acc67ec73f01932fa2ddb1e236a78
girst <girst@users.noreply.github.com>
2020-01-18 19:35:02 +0100

    Fix addon installation for Firefox 74
    regressed by: Bug 1524327

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 // keep this comment
 try {
   let Xdb = Cu.import('resource://gre/modules/addons/XPIDatabase.jsm', {});
+  Xdb.XPIDatabase.isDisabledLegacy = (addon) => false;
   Xdb.XPIDatabase['SIGNED_TYPES'].clear();
   Xdb.AddonSettings = {
     "REQUIRE_SIGNING": false,
     "LANGPACKS_REQUIRE_SIGNING": false,
-    "ALLOW_LEGACY_EXTENSIONS": true,
+    "ALLOW_LEGACY_EXTENSIONS": true, // <=fx73
+    "EXPERIMENTS_ENABLED": true, // >=fx74
+    "DEFAULT_THEME_ID": "default-theme@mozilla.org",
   };
 
   const {FileUtils} = Cu.import('resource://gre/modules/FileUtils.jsm');
