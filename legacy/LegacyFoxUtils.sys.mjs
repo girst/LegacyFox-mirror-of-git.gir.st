3b7740f4da0ad4e8c21792fd6215c5c5a389bfe4
girst <girst@users.noreply.github.com>
2025-08-22 16:39:12 +0200

    keep consistent manifest file name
    removeBootstrappedManifestLocation is not called on exit, only when
    disabling the addon. hence, restarting the browser would accumulate a
    bunch of old manifests. onemen/117649 get around this by removing the
    manifest on exit, but their code (following) is not working for me:
        Cc['@mozilla.org/uriloader/external-helper-app-service;1']
          .getService(Ci.nsPIExternalAppLauncher)
          .deleteTemporaryFileOnExit(tempDir);
    
    partially reverts 7c2fc6e40e8e84d9cecde02e9630318ccca605ab.
    
    additional note: our removeBootstrappedManifestLocation removes the
    chrome.manifest (instead of truncating it with manifest.fileSize = 0).
    this causes "Could not read chrome manifest 'file:///...'" to get
    console.log'ged. this appears to be harmless and not worth fixing.

--- legacy/LegacyFoxUtils.sys.mjs
+++ legacy/LegacyFoxUtils.sys.mjs
@@ -47,7 +47,7 @@ export class LegacyFoxUtils {
     manifest.append('legacy-extension-data');
     manifest.append(addon.id);
     manifest.exists() || manifest.create(Ci.nsIFile.DIRECTORY_TYPE, 0o755);
-    manifest.append(`${Date.now()}.manifest`); /* created or truncated by ostream */
+    manifest.append('chrome.manifest'); /* created or truncated by ostream */
 
     // write modified chrome.manifest to profile directory
     let ostream = new FileOutputStream(manifest, -1, -1, 0);
